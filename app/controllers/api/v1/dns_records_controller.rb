9ae325d2dfb8a02c2b2ca5d918f48a4f73995bc3
Marcio <marcitolopes@gmail.com>
2019-05-16 14:34:31 -0300

    Add DNSController#index
    This will allow to filter records the follow criteria:
      1. include_hostnames
      2. exclude_hostnames
      3. page (for pagination)
    
    The expected return will be the follow json as example:
    
    ```
    {
      "total_matching_dns_records": 2,
      "matching_dns_records": [
        {"id":1, "ip_address":"1.1.1.1"},
        {"id":2, "ip_address":"2.2.2.2"}
      ],
      associated_hostnames: {
        "local.one":1,
        "local.two":2
      }
    }
    ```

--- app/controllers/api/v1/dns_records_controller.rb
+++ app/controllers/api/v1/dns_records_controller.rb
@@ -3,15 +3,39 @@ class Api::V1::DNSRecordsController < ApplicationController
     @dns_record = DNSRecord.new(dns_record_params)
     if @dns_record.save
       render json: {
-        data: {
-          id: @dns_record.id
-        },
+        id: @dns_record.id
       }, status: :ok
     else
       render json: {
         errors: @dns_record.errors.full_messages
       }, status: :unprocessable_entity
     end
+  end
+
+  def index
+    result = DNSRecord.filter_records(
+      include_hostnames: params[:include_hostnames],
+      exclude_hostnames: params[:exclude_hostnames],
+      page: params[:page].to_i == 0 ? 1 : params[:page].to_i
+    )
+
+    total = result.total
+    records = result.records
+
+    matching_dns_records =
+      records.map { |r| {id: r.id, ip_address: r.ip_address.to_s} }
+
+    associated_hostnames =
+      records
+        .flat_map(&:hostnames)
+        .map(&:name)
+        .reduce(Hash.new(0)) { |h, v| h[v] += 1; h }
+
+    render json: {
+      total_matching_dns_records: total,
+      matching_dns_records: matching_dns_records,
+      associated_hostnames: associated_hostnames
+    }
   end
 
   private
--- test/controllers/api/v1/dns_records_controller_test.rb
+++ test/controllers/api/v1/dns_records_controller_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class Api::V1::DNSRecordsControllerTest < ActionDispatch::IntegrationTest
-  test "creates a dns_record with associations" do
+  test "POST /api/v1/dns_records - creates a dns_record with associations" do
     dns_record_params = {
       dns_record: {
         ip_address: '80.104.176.249',
@@ -16,10 +16,10 @@ class Api::V1::DNSRecordsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
 
     record = DNSRecord.where(ip_address: '80.104.176.249').first
-    assert JSON.parse(@response.body, symbolize_names: true) == {data: {id: record.id}}
+    assert JSON.parse(@response.body, symbolize_names: true) == {id: record.id}
   end
 
-  test "returns 422 status with error message" do
+  test "POST /api/v1/dns_records - returns 422 status with error message" do
     dns_record_params = {
       dns_record: {
         ip_address: 'a.b.c.d',
@@ -39,5 +39,85 @@ class Api::V1::DNSRecordsControllerTest < ActionDispatch::IntegrationTest
         "Ip address can't be blank"
       ]
     }
+  end
+
+  test "GET /api/v1/dns_records - returns only records filtered by given criteria" do
+    create_dns_record ip_address: '1.1.1.1', hostnames: ['lorem.com', 'ipsum.com', 'dolor.com', 'amet.com']
+    create_dns_record ip_address: '2.2.2.2', hostnames: ['ipsum.com']
+    create_dns_record ip_address: '3.3.3.3', hostnames: ['ipsum.com', 'dolor.com', 'amet.com']
+    create_dns_record ip_address: '4.4.4.4', hostnames: ['ipsum.com', 'dolor.com', 'sit.com', 'amet.com']
+    create_dns_record ip_address: '5.5.5.5', hostnames: ['dolor.com', 'sit.com']
+
+    get api_v1_dns_records_url, params: {
+      include_hostnames: ['ipsum.com', 'dolor.com'],
+      exclude_hostnames: ['sit.com'],
+      page: 1
+    }
+
+    records = DNSRecord.where(
+      ip_address: ['1.1.1.1', '3.3.3.3']
+    )
+
+    assert_response :success
+
+    response = JSON.parse(@response.body)
+    assert response["total_matching_dns_records"] == 2
+    assert response["matching_dns_records"].sort_by {
+      |m| m["id"]
+    } == [
+      {
+        "id" => records[0].id,
+        "ip_address" => '1.1.1.1'
+      },
+      {
+        "id" => records[1].id,
+        "ip_address" => '3.3.3.3'
+      }
+    ]
+    assert response["associated_hostnames"] ==
+      records
+        .flat_map(&:hostnames)
+        .map(&:name)
+        .reduce(Hash.new(0)) { |h, v| h[v] += 1; h }
+  end
+
+  test "GET /api/v1/dns_records - returns all records without the optional parameters" do
+    get api_v1_dns_records_url
+
+    records = DNSRecord.all.sort_by { |r| r.ip_address.to_s }
+
+    assert_response :success
+
+    response = JSON.parse(@response.body)
+    assert response["total_matching_dns_records"] == 3
+    response["matching_dns_records"].sort_by { |r|
+      r["ip_address"]
+    } == [
+      {
+        "id" => records[0].id,
+        "ip_address" => '0.0.0.0'
+      },
+      {
+        "id" => records[1].id,
+        "ip_address" => '127.0.0.1'
+      },
+      {
+        "id" => records[2].id,
+        "ip_address" => '8.8.8.8'
+      }
+    ]
+    assert response["associated_hostnames"] == {
+      "local.one" => 2,
+      "local.two" => 2
+    }
+  end
+
+  def create_dns_record(ip_address:, hostnames:)
+    DNSRecord.create(
+      ip_address: ip_address,
+      hostnames: hostnames.map do |name|
+        Hostname.find_or_initialize_by(name: name)
+      end
+    )
   end
 end
